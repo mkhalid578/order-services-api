e3b807fd27965592ea676e657e3784802482e8b6
Muhammed Khalid <mkhalid@Uzairs-MacBook-Air.local>
2017-09-17 11:50:05 -0400

    Updated *.py file

--- deploy.py
+++ deploy.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route('/')
 def index():
@@ -8,3 +12,6 @@ def index():
 @app.route('/data')
 def data():
     return "{'employee':'David Adams'}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
