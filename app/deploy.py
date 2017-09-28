fb77d37cfae81a4d633e7db790ed8bd75cfbdada
Muhammed Khalid <mkhalid@Uzairs-MacBook-Air.local>
2017-09-28 16:05:29 -0400

    updated deploy

--- app/deploy.py
+++ app/deploy.py
@@ -9,3 +9,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",config.SQL_DATABASE_URI)
 db = SQLAlchemy(app)
 api = Api(app)
+
+@app.route("/")
+def index():
+    return "<h1> Stuff </h1>"
