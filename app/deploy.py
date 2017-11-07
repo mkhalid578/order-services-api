cf0dd5f1038b314a83691eba3aeeaf7acd165ee5
Muhammed Khalid <mkhalid@Uzairs-MacBook-Air.local>
2017-11-07 13:03:41 -0500

    Added dashboard template

--- app/deploy.py
+++ app/deploy.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from json import dumps
@@ -12,9 +12,19 @@ api = Api(app)
 
 @app.route("/")
 def startPage():
-	return "<h1> Default Page </h1>"
+	return render_template("index.html")
 
-@app.route("/web")
-def index():
-    return render_template("index.html")
+@app.route("/web/login", methods=['GET', 'POST'])
+def login():
+        error = None
+        if request.method == "POST":
+                if request.form['email'] == "test@gmail.com" and request.form['password'] == 'password':
+                        return redirect(url_for('dashboard'))
+                else:
+                        error = "Invalid Username or Password"
 
+        return render_template("index.html", error=error)
+
+@app.route("/web/dashboard")
+def dashboard():
+	return render_template("dashboard.html")
